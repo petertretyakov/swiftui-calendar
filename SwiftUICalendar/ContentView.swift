a69cf9521761928da5275bffc73b5a6c989a5fbc
petertretyakov <petertretyakov@gmail.com>
2020-04-15 01:10:19 +0300

    Initial Commit

---
+++ SwiftUICalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalendar
+//
+//  Created by Peter Tretyakov on 15.04.2020.
+//  Copyright © 2020 Peter Tretyakov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
